472b2c10656eee1ec58bc57ca4cf01d97f899ae0
hangkuyr <laisglima@hotmail.com>
2022-06-21 21:19:10 -0300

    Update to Firefox

--- Project/tests/test_e2e.py
+++ Project/tests/test_e2e.py
@@ -1,38 +1,14 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def test_first_case_selenium():
 
-	display = Display(visible=0, size=(800, 800))  
-	display.start()
-
-	chromedriver_autoinstaller.install()
-
-	chrome_options = webdriver.ChromeOptions()    
-	# Add your options as needed    
-	options = [
-	  # Define window size here
-	   "--window-size=1200,1200",
-	    "--ignore-certificate-errors"
-	 
-	    "--headless",
-	    #"--disable-gpu",
-	    #"--ignore-certificate-errors",
-	    #"--disable-extensions",
-	    "--no-sandbox",
-	    "--disable-dev-shm-usage",
-	    #'--remote-debugging-port=9222'
-	]
-
-	for option in options:
-	    chrome_options.add_argument(option)
-
-    
-	driver = webdriver.Chrome(options = chrome_options)
+	options = FirefoxOptions()
+	options.add_argument("--headless")
+	driver = webdriver.Firefox()
 	
-	driver.get("http://localhost:8080")
+	driver.get("http://127.0.0.1:80")
 	assert "Upload File" in driver.title
+
+	driver.quit()
